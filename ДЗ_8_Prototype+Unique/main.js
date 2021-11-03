01e1749d393d81936d9eb3ca2484a6c759ee0d8d
Ivashin-Max <ivashinmax@gmail.com>
2021-11-03 20:38:31 +0300

    Реализация unique

--- ДЗ_8_Prototype+Unique/main.js
+++ ДЗ_8_Prototype+Unique/main.js
@@ -1,8 +1,37 @@
-console.logTime = console.log.bind(console);
 
-console.log = function (...data) {
-	let time = `Время (${new Date().toLocaleTimeString()}) `;
-	this.logTime(time, ...data);
-};
-let q = 'wrewerw'
-console.log('Проверка логгера', q, 123123);
+
+
+
+
+
+//Unique------------------------------
+let simpleArr = [1, 2, 3, 3, 4, 6, '11', '11', '2'];
+let objectArr = [{ id: 1, name: 'Vasya' }, { id: 2, name: 'Petya' }, { age: 18, name: 'Max' }, { age: 19, name: 'Max' }, { age: 146, name: 'Max' },]
+
+
+
+Array.prototype.unique = function () {
+	if (!arguments.length) return [...new Set(this)];
+
+	const uniqueKey = arguments[0];
+
+	let firstUniqueObj;
+	for (let i = 0; i < this.length; i++) {
+		const element = this[i];
+		if (element.hasOwnProperty(uniqueKey)) {
+			firstUniqueObj = element;
+
+			break
+		}
+	}
+	let filtredArr = this.filter(el => !el.hasOwnProperty(uniqueKey))
+	let resultArr = [firstUniqueObj, ...filtredArr]
+	return resultArr
+
+}
+console.log(`Unique_test---`);
+console.log(`Исходный массив примитивов`, simpleArr);
+console.log(`Исходный "сложный" массив `, objectArr);
+console.log(`Возврат уникального массива из примитивов`, simpleArr.unique());
+console.log(`Возврат уникального массива из "сложного" массива по ключу id`, objectArr.unique('id'));
+console.log(`Возврат уникального массива из "сложного" массива по ключу name`, objectArr.unique('age'));
