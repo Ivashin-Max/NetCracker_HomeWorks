359a54e1b4e6848ffc33c26d95cbe71f512506ed
Ivashin-Max <ivashinmax@gmail.com>
2021-11-08 20:43:54 +0300

    Proto

--- ДЗ_8_Prototype+Unique/main.js
+++ ДЗ_8_Prototype+Unique/main.js
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 //Unique------------------------------
 let simpleArr = [1, 2, 3, 3, 4, 6, '11', '11', '2'];
 let objectArr = [{ id: 1, name: 'Vasya' }, { id: 2, name: 'Petya' }, { age: 18, name: 'Max' }, { age: 19, name: 'Max' }, { age: 146, name: 'Max' },]
@@ -35,3 +30,67 @@ console.log(`Исходный "сложный" массив `, objectArr);
 console.log(`Возврат уникального массива из примитивов`, simpleArr.unique());
 console.log(`Возврат уникального массива из "сложного" массива по ключу id`, objectArr.unique('id'));
 console.log(`Возврат уникального массива из "сложного" массива по ключу name`, objectArr.unique('age'));
+
+
+
+//Prototype-------------------------
+console.log('Prototype test------------');
+function inherit(ParentClass) {
+	function ChildClass() { }
+	ChildClass.prototype = Object.create(ParentClass.prototype);
+	ChildClass.prototype.constructor = ChildClass;
+	ChildClass.prototype._super = ParentClass.prototype;
+	return ChildClass;
+}
+
+function Parent() { };
+Parent.prototype.init = function (name) {
+	this.name = name;
+};
+
+Parent.prototype.print = function () {
+	console.log(this.name);
+};
+
+//Функция наследования из презентации
+let Child = inherit(Parent);
+Child.prototype.init =
+	function (name) {
+		name = 'Mr. ' + name;
+		//переопределние метода через супер
+		this._super.init.call(this, name);
+	}
+
+let max = new Child();
+
+max.init('Max');
+
+max.print();
+
+
+//Вложенность
+
+let first = {
+	name: 'First',
+	sayHi() {
+		console.log("Hi,", this.name);
+	}
+}
+
+let second = {
+	age: 23,
+	__proto__: first
+};
+
+let third = {
+	isPretty: true,
+	__proto__: second
+}
+
+//Третий наследует от первого
+third.sayHi()
+//И от второго тоже
+console.log(third.age);
+
+
+
