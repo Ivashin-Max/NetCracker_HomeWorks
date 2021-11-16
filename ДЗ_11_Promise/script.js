0806072656ee1cea38ae887c06cb1f6ba3025081
Ivashin-Max <ivashinmax@gmail.com>
2021-11-16 10:29:52 +0300

    Promise 2.0

--- ДЗ_11_Promise/script.js
+++ ДЗ_11_Promise/script.js
@@ -1,14 +1,41 @@
 
-let promise = Promise.resolve()
+'use strict'
 
-promise
-	.then(() => {
-		let q = 1
-		alert(`Первый then, значение переменной равно ${q}`)
-		return q
-	})
-	.then(res => {
-		alert(`Это второй then, выводим значение переменной из первого then:${res}`)
-		return res
-	})
-	.then(res => alert(`Это третий then, выводим значение из второго then:${res}`))
+function isCarCheap() {
+	const CHEAP_PRICE = 5e6
+	let carPrice = Math.floor((Math.random() * 1e7));
+	console.log(`Цена машины: ${carPrice} рублей`);
+	carPrice <= CHEAP_PRICE ? console.log('Могу себе позволить') : console.log('Машина дорогая');
+	if (carPrice <= CHEAP_PRICE) return true
+	else return false
+}
+
+const willIGetNewCar = new Promise(
+	(resolve, reject) => {
+		if (isCarCheap()) {
+			const car = {
+				brand: 'BMW',
+				model: '5'
+			};
+			resolve(car);
+		} else {
+			const reason = new Error('Не могу себе позволить');
+			reject(reason);
+		}
+
+	}
+);
+
+const showCar = function (car) {
+	const message = 'Я купил новую машину: ' + car.brand + ' ' + car.model;
+	return Promise.resolve(message);
+};
+
+
+const checkCarPrice = function () {
+	willIGetNewCar
+		.then(showCar)
+		.then(resolved => console.log(resolved))
+		.catch(error => console.log(error.message));
+};
+checkCarPrice();
